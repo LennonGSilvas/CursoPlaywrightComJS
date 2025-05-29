1161e4406516b5eb082c91ed2df8a045d280ef33
Lennon Giovane da Silva <lennongiovane28@gmail.com>
2025-05-29 00:27:23 -0300

    trabalhando com localizadores e expect

--- tests/PaginaDeProdutos.spec.js
+++ tests/PaginaDeProdutos.spec.js
@@ -2,7 +2,23 @@ import {test, expect} from '@playwright/test'
 
 test('PaginaDeProdutos', async ({page})=> {
 
+    //acessa a pagina
     await page.goto("http://localhost:2221/");
     await expect(page).toHaveTitle('Art Shopping Store')
+    await page.pause()
+
+    //Adiciona os produtos 
+    const adicionarProdutos = page.locator('[data-qa="product-button"]').first()
+    await adicionarProdutos.waitFor()
+    await expect(adicionarProdutos).toHaveText('Add to Basket')
+    await adicionarProdutos.click()
+    await adicionarProdutos.waitFor()
+    await expect(adicionarProdutos).toHaveText("Remove from Basket")
+
+    //Vai até a pagina do checkout
+    const acessaPaginaCheckout = page.getByRole('link', { name: 'Checkout' })
+    await acessaPaginaCheckout.waitFor()
+    await acessaPaginaCheckout.click()
+    await page.waitForURL("http://localhost:2221/basket")
 
 })
